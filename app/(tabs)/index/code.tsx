7f11448d5614ca6a24fd532857e4170a0d52e80d
Anton Max Mittmann <antmitt04@gmail.com>
2025-10-07 13:44:40 +0200

    Code page UI done.

--- app/(tabs)/index/code.tsx
+++ app/(tabs)/index/code.tsx
@@ -1,7 +1,7 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Picker } from "@react-native-picker/picker";
 import React from "react";
-import { TextInput, View } from "react-native";
+import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const CodeComponent = () => {
   return (
@@ -16,11 +16,39 @@ const CodeComponent = () => {
       <View className="flex items-center mt-2">
         <Ionicons name="arrow-down" size={24} />
       </View>
+
       <View>
         <Picker>
           <Picker.Item label="Text zu Zahlen" />
           <Picker.Item label="Test" />
         </Picker>
+      </View>
+
+      <View className="w-[80%] flex flex-row">
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores velit
+          accusantium praesentium corporis dignissimos ut nihil fugiat molestias
+          non rerum! Placeat animi debitis exercitationem atque dicta error aut
+          eaque deleniti.
+        </Text>
+        <View className="flex flex-col justify-between ml-3">
+          <TouchableOpacity
+            onPress={() => {
+              Alert.alert("Copy");
+            }}
+            className="bg-blue-200 w-12 h-12 rounded-full flex items-center justify-center p-2"
+          >
+            <Ionicons name="copy-outline" size={24} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              Alert.alert("Share");
+            }}
+            className="bg-blue-200 w-12 h-12 rounded-full flex items-center justify-center p-2"
+          >
+            <Ionicons name="share-outline" size={24} />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
