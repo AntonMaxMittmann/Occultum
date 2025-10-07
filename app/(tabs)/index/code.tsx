b0019a8156408e6c4dbbc506bc72a821632bcda1
Anton Max Mittmann <antmitt04@gmail.com>
2025-10-07 13:38:24 +0200

    Picker implemented.

--- app/(tabs)/index/code.tsx
+++ app/(tabs)/index/code.tsx
@@ -1,14 +1,27 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Picker } from "@react-native-picker/picker";
 import React from "react";
 import { TextInput, View } from "react-native";
 
 const CodeComponent = () => {
   return (
-    <View>
+    <View className="flex flex-col">
       <TextInput
         placeholder="Text in normaler Schrift..."
-        className="bg-gray-400 p-2 rounded-lg h-64"
+        className="bg-blue-200 p-2 rounded-lg h-28"
         multiline={true}
+        scrollEnabled={true}
       />
+
+      <View className="flex items-center mt-2">
+        <Ionicons name="arrow-down" size={24} />
+      </View>
+      <View>
+        <Picker>
+          <Picker.Item label="Text zu Zahlen" />
+          <Picker.Item label="Test" />
+        </Picker>
+      </View>
     </View>
   );
 };
