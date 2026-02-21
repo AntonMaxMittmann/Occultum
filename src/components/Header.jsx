ad3ea7b04fc1d2a0f9d0c6e82ab05c1f1fdb16a9
antmitt <antmitt@icloud.com>
2026-02-21 16:53:37 +0000

    Icon hinzugefügt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import icon from "../assets/Occultum Icon Light.png";
 export default function Header() {
   return (
-    <div className="flex justify-between items-center bg-blue-800 p-5">
+    <div className="flex justify-start items-center bg-blue-800 p-5">
+      <img src={icon} alt="" className="h-10 mr-3" />
       <h1 className="text-xl">Occultum</h1>
     </div>
   );
