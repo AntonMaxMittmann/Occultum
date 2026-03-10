67a8d3d5dd52003bc906b9fd24787ebac2cc0c9e
antmitt <antmitt@icloud.com>
2026-03-10 13:46:48 +0000

    Verschlüsseln und Entschlüsseln Tabs

--- src/routes/Code.jsx
+++ src/routes/Code.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+
 export default function Code() {
-  return <h1>Code</h1>;
+  const [page, setPage] = useState(1);
+  const bg1 = page == 1 ? "#3399ff" : "#d1e0e0";
+  const bg2 = page == 2 ? "#3399ff" : "#d1e0e0";
+  return (
+    <div className="flex items-center justify-center mt-5">
+      <button
+        onClick={() => setPage(1)}
+        style={{ backgroundColor: bg1 }}
+        className="mr-4 p-2 rounded-sm cursor-pointer"
+      >
+        Verschlüsseln
+      </button>
+      <button
+        onClick={() => setPage(2)}
+        style={{ backgroundColor: bg2 }}
+        className="mr-4 p-2 rounded-sm cursor-pointer"
+      >
+        Entschlüsseln
+      </button>
+    </div>
+  );
 }
